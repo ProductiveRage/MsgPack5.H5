f9cd3b99119fcae039def32c342541685c4709b5
Dan Roberts <dangger36@gmail.com>
2020-07-25 17:38:35 +0100

    Remove final advisories

--- Tests/SharedTestItems/TestItemInstanceCreator.cs
+++ Tests/SharedTestItems/TestItemInstanceCreator.cs
@@ -13,7 +13,9 @@ namespace MessagePack.Tests.SharedTestItems
             if (testItemType is null)
                 throw new ArgumentException("does not relate to an accessible type: " + testItemTypeName, nameof(testItemTypeName));
 
+#pragma warning disable CA1825 // 2020-07-25 DWR: Disable "Avoid unnecessary zero - length array allocations. Use Array.Empty<Type>()" because H5 doesn't support that method and this class is shared by both .NET and H5 projects
             var ctor = testItemType.GetConstructor(new Type[0]);
+#pragma warning restore CA1825
             if (ctor is null)
                 throw new ArgumentException("does not have an accessible parameter-less constructor: " + testItemTypeName, nameof(testItemTypeName));
 
