bdf34acf6f1bb8e86392c65a20479e4190d428a7
Dan Roberts <dangger36@gmail.com>
2020-07-25 17:00:50 +0100

    Rename the primary MsgPack5.H5 project to MessagePage.H5 (though keeping the solution and repo as MsgPack.H5 to recognise its roots and the primary namespace as simply "MessagePack" for shared code convenience)

--- Tests/UnitTestDataGenerator/Program.cs
+++ Tests/UnitTestDataGenerator/Program.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using MessagePack;
-using MsgPack5.H5.Tests.SharedTestItems;
+using MessagePack.Tests.SharedTestItems;
 
 namespace UnitTestDataGenerator
 {
