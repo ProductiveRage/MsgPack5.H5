09eb37cd3229faa4635df69eb4508aa8ce17ea61
Dan Roberts <dangger36@gmail.com>
2020-08-04 13:12:09 +0100

    Add a test around the failure behaviour of trying to deserialise from an int to a string (some revisions ago, this worked in this code but it doesn't in the .NET library and so it shouldn't here for consistency)

---
+++ Tests/SharedTestItems/Failures/CanNotConvertPropertyFromIntToString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MessagePack.Tests.SharedTypes;
+
+namespace MessagePack.Tests.SharedTestItems.Failures
+{
+    internal sealed class CanNotConvertPropertyFromIntToString : ITestItem
+    {
+        public Type SerialiseAs => typeof(ClassWithByteProperty);
+        public Type DeserialiseAs => typeof(ClassWithStringProperty);
+        public object Value => new ClassWithByteProperty { Key = 12 };
+    }
+}
---
+++ Tests/SharedTypes/ClassWithStringProperty.cs
@@ -0,0 +1,9 @@
+﻿namespace MessagePack.Tests.SharedTypes
+{
+    [MessagePackObject]
+    public sealed class ClassWithStringProperty // Note: Must be public (not internal) to work with MessagePack
+    {
+        [Key(0)]
+        public string Key { get; set; }
+    }
+}
