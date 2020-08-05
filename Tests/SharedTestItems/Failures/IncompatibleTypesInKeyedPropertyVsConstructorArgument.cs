02997beb1ee6ffbf1b8c9257a9eaaeeee8d10447
Dan Roberts <dangger36@gmail.com>
2020-08-05 17:19:51 +0100

    Add a failure test for the case where there is a constructor but its parameter type doesn't match the key'd property

---
+++ Tests/SharedTestItems/Failures/IncompatibleTypesInKeyedPropertyVsConstructorArgument.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MessagePack.Tests.SharedTypes;
+
+namespace MessagePack.Tests.SharedTestItems.Failures
+{
+    internal sealed class IncompatibleTypesInKeyedPropertyVsConstructorArgument : ITestItem
+    {
+        public Type SerialiseAs => typeof(ClassWithIntProperty); // Have to serialise as this type because the .NET library will refuse to even serialise a ClassWithIntPropertySetByConstructorWithIncompatibleType instance
+        public Type DeserialiseAs => typeof(ClassWithIntPropertySetByConstructorWithIncompatibleType);
+        public object Value => new ClassWithIntProperty { Key = 123 };
+    }
+}
---
+++ Tests/SharedTypes/ClassWithIntPropertySetByConstructorWithIncompatibleType.cs
@@ -0,0 +1,14 @@
+﻿namespace MessagePack.Tests.SharedTypes
+{
+    /// <summary>
+    /// The .NET library will not be happy with this, so it should refuse to serialise it
+    /// </summary>
+    [MessagePackObject]
+    public sealed class ClassWithIntPropertySetByConstructorWithIncompatibleType // Note: Must be public (not internal) to work with MessagePack
+    {
+        public ClassWithIntPropertySetByConstructorWithIncompatibleType(string value) => ValueLength = value?.Length ?? -1;
+        
+        [Key(0)]
+        public int ValueLength { get; }
+    }
+}
